d231cbe892a8ee364ab7812c3f6acd985f45dd5c
MasonNguyen1223 <masonb.n23@gmail.com>
2025-12-09 16:13:48 -0600

    added code so that images are accessible to code

--- my-app/src/assets/images.js
+++ my-app/src/assets/images.js
@@ -1,13 +1,38 @@
 import ClassicMilkTea from "./Classic Pearl Milk Tea.webp";
 import CoffeeCreama from "./Coffee Creama.avif";
 import CoffeeMilkTeaWithCoffeeJelly from "./Coffee Milk Tea with Coffee Jelly.webp";
+import boba from"./boba.avif";
+import GoldenRetriever from "./Golden Retriever.webp";
+import HokkaidoPearlMilkTea from "./Hokkaido Pearl Milk Tea.webp";
+import HoneyBlackTea from "./Honey Black Tea.avif";
+import HoneyGreenTea from "./Honey Green Tea.webp";
+import HoneyPearlMilkTea from "./Honey Pearl Milk Tea.avif";
+import MangoPassionFruitGreenTea from "./Mango & Passion Fruit Green Tea.avif";
+import MangoGreenMilkTea from "./Mango Green Milk Tea.webp";
+import MatchaPearlMilkTea from "./Matcha Pearl Milk Tea.avif";
+import OolongGreenTea from "./Oolong Green Tea.avif";
+import TaroPearlMilkTea from "./Taro Pearl Milk Tea.avif";
+import ThaiPearlMilkTea from "./Thai Pearl Milk Tea.avif";
+import WintermelonMilkTea from "./Wintermelon Milk Tea.webp";
 import ReactSVG from "./react.svg";
 
 export const itemImages = {
   "classic pearl milk tea": ClassicMilkTea, // lowercase
   "coffee creama": CoffeeCreama,
   "coffee milk tea with coffee jelly": CoffeeMilkTeaWithCoffeeJelly,
+  "golden retriever": GoldenRetriever,
+  "hokkaido pearl milk tea": HokkaidoPearlMilkTea,
+  "honey black tea": HoneyBlackTea,
+  "honey green tea": HoneyGreenTea,
+  "honey pearl milk tea": HoneyPearlMilkTea,
+  "mango & passion fruit green tea": MangoPassionFruitGreenTea,
+  "mango green milk tea": MangoGreenMilkTea,
+  "matcha pearl milk tea": MatchaPearlMilkTea,
+  "oolong green tea": OolongGreenTea,
+  "taro pearl milk tea": TaroPearlMilkTea,
+  "thai pearl milk tea": ThaiPearlMilkTea,
+  "wintermelon milk tea": WintermelonMilkTea,
   react: ReactSVG,
 };
 
-export const fallbackImage = ReactSVG;
+export const fallbackImage = boba;
